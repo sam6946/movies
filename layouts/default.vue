1661dbfdc1c1c515656b6d03d8efee98372d9bef
sam6946 <samtaka5642@gmail.com>
2022-05-01 23:57:44 +0100

    first-commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app"> 
+      <Nuxt />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DefaultsLayout'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
